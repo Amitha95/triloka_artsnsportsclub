eb462fe5aee101f0abd6625aed0294fd728193ba
Amitha95 <99869312+Amitha95@users.noreply.github.com>
2025-02-12 13:51:26 +0530

    Modified Media URLs

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
-from .views import home
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewSet, GalleryImageListView, MemberImageListView
+
+router = DefaultRouter()
+router.register(r'projects', ProjectViewSet)
 
 urlpatterns = [
-    path('', home, name='home'),  # Add this
-    
+    path('api/', include(router.urls)),  # Include all registered viewsets
+    path('api/gallery-images/', GalleryImageListView.as_view(), name='gallery-image-list'),
+    path('api/member-images/', MemberImageListView.as_view(), name='member-image-list'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,46 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
+from django.conf import settings
+import os
+from urllib.parse import urljoin
+from .models import Project, GalleryImage, Member
+from .serializers import ProjectSerializer, GalleryImageSerializer, MemberSerializer
 
-def home(request):
-    return JsonResponse({"message": "Welcome to the API"})
+# Project ViewSet
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+# Gallery Image API View
+class GalleryImageListView(APIView):
+    permission_classes = [AllowAny]  
+
+    def get(self, request):
+        media_url = settings.MEDIA_URL
+        gallery_images_dir = os.path.join(settings.MEDIA_ROOT, 'gallery_images')
+
+        if os.path.isdir(gallery_images_dir):
+            image_files = [f for f in os.listdir(gallery_images_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+            image_urls = [urljoin(media_url, f'gallery_images/{image}') for image in image_files]
+        else:
+            image_urls = []
+
+        return Response({'images': image_urls})
+
+# Member Image API View
+class MemberImageListView(APIView):
+    permission_classes = [AllowAny]  
+
+    def get(self, request):
+        media_url = settings.MEDIA_URL
+        member_images_dir = os.path.join(settings.MEDIA_ROOT, 'member_images')
+
+        if os.path.isdir(member_images_dir):
+            image_files = [f for f in os.listdir(member_images_dir) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+            image_urls = [urljoin(media_url, f'member_images/{image}') for image in image_files]
+        else:
+            image_urls = []
+
+        return Response({'images': image_urls})
