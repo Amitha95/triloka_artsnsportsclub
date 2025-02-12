27522a1cc59acf6da4b1f955ff35b46dea1e2a07
Amitha95 <99869312+Amitha95@users.noreply.github.com>
2025-02-12 13:58:47 +0530

    Modified Media URLs

--- backend/urls.py
+++ backend/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),  # Include the API URLs
+    path('api/', include('api.urls')),  # Include the API URLs
 ]
 
 # Serve media files in development
